cbd4024338ccbcc90ad32f106b9ada17956690c9
MikolajRybak <mysteeer@gmail.com>
2022-09-27 13:48:15 +0200

    done

---
+++ Scripts/scripts.js
@@ -0,0 +1,36 @@
+function openNav() {
+    document.getElementById("menu").style.width = "15vh";
+  }
+  
+  function closeNav() {
+                document.getElementById("menu").style.width = "0";
+              }
+function openSkill1(){
+  document.getElementById("load1").style.width = "50%";
+
+  document.getElementById("load2").style.width = "25%";
+
+  document.getElementById("load3").style.width = "20%";
+
+  document.getElementById("load4").style.width = "20%";
+
+  document.getElementById("load5").style.width = "90%";
+
+  document.getElementById("load6").style.width = "60%";
+
+  document.getElementById("load7").style.width = "90%";
+
+  document.getElementById("load8").style.width = "70%";
+
+  document.getElementById("load9").style.width = "10%";
+
+  document.getElementById("load10").style.width = "10%";
+
+  document.getElementById("load11").style.width = "95%"; 
+
+  document.getElementById("load12").style.width = "80%";
+
+  document.getElementById("load13").style.width = "50%";
+
+
+}
